383887616ffa0567f21fd3b89a0ac4b0d0d78f7e
Florian CHAPUIS <fchapuis20@gmail.com>
2024-11-13 14:53:57 +0100

    Exercice terminé

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
